5bd7d2cf8fb4b59140e82cd7377ed91ce4f9aecc
sirishaprathipati <sirishaprathipati365@gmail.com>
2023-01-08 07:54:19 +0530

    Added factorial.py

---
+++ factorial.py
@@ -0,0 +1,12 @@
+ch=int(input('Enter a Number to find Factorial:'))
+def factorial(num):
+    f=1
+    if num>0:
+        for i in range(1,num+1):
+            #print(i)
+            f=f*i
+        print(f)
+    else :
+        print('Please Enter correct input')
+
+factorial(ch)
